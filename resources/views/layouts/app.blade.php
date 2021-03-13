3b8b6b15d3ef9e43c7f8728da8acc259dec4d2ce
Grant Holley <gvholley@gmail.com>
2021-03-13 21:02:48 +0900

    Added Username to Users

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,24 @@
         <a href="" class="p-3">Post</a>
       </li>
     </ul>
+
+    <ul class="flex items-center">
+      <li>
+        <a href="" class="p-3">Grant</a>
+      </li>
+
+      <li>
+        <a href="" class="p-3">Login</a>
+      </li>
+
+      <li>
+        <a href="" class="p-3">Register</a>
+      </li>
+
+      <li>
+        <a href="" class="p-3">Logout</a>
+      </li>
+    </ul>
   </nav>
   @yield('content')
 </body>
