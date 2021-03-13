b27b0cb972a3102f9c78a107577f12ec0da8c375
Grant Holley <gvholley@gmail.com>
2021-03-13 20:52:15 +0900

    Added Tailwind

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Post!</title>
+<link href="{{ asset('css/app.css') }}" rel="stylesheet" />
+</head>
+<body class="bg-gray-100">
+  <nav class="p-6 bg-white flex justify-between">
+    <ul class="flex items-center">
+      <li>
+        <a href="" class="p-3">Home</a>
+      </li>
+
+      <li>
+        <a href="" class="p-3">Dashboard</a>
+      </li>
+
+      <li>
+        <a href="" class="p-3">Post</a>
+      </li>
+    </ul>
+  </nav>
+  @yield('content')
+</body>
+</html>
