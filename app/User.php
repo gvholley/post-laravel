265e0ac82af502426fe5b726d31e08ae1b11f2eb
Grant Holley <gvholley@gmail.com>
2021-03-14 19:51:43 +0900

    Working on DB

--- app/User.php
+++ app/User.php
@@ -15,8 +15,11 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $fillable = [
-        'name', 'email', 'password', 'username'
+protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'username',
     ];
 
     /**
